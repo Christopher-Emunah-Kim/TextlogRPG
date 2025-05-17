bd2ff675ea3930274345596089e5011060080ea1
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-18 00:42:07 +0900

    25.05.17_06 UX Update
    - UI 표현속도 조절
    - UX 사항 일부 개선
      -> 메인 로비-> 게임 시작 코멘터리 변경
      -> 방 이동시 직접 주소정보를 주지않도록 변경

--- TextlogRPG/TextlogRPG/Player/Player.cpp
+++ TextlogRPG/TextlogRPG/Player/Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.h"
 #include "../Map/Room.h"
+#include "Windows.h" 
 #include <iostream>
 
 using namespace std;
@@ -13,10 +14,14 @@ void Player::PlayerMove(Direction dir)
 	if (targetRoom != nullptr)
 	{
 		currentRoom = targetRoom;
-		cout << "당신은" << currentRoom->getPositionX() << ", " << currentRoom->getPositionY() << " 방으로 이동했습니다." << endl;
+		Sleep(1000);
+		cout << "당신은 천천히 걸음을 옮겨 새로운 방으로 이동했습니다." << endl;
+		Sleep(2000);
 	}
 	else
 	{
+		Sleep(1000);
 		cout << "당신의 앞은 단단한 벽으로 막혀있습니다. 다른 방향을 선택하세요" << endl;
+		Sleep(1000);
 	}
 }
