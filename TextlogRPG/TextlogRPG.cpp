f624e115c823cc3f16e20659ed22045427bd4240
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-17 22:14:26 +0900

    25.05.17_02. Class Generating
    - class Map, Room, Player Generating.
    - Default Draft

--- TextlogRPG/TextlogRPG.cpp
+++ TextlogRPG/TextlogRPG.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
-    
+	//플레이할 맵 생성
+	//플레이어 생성
+	
+	// RPG 게임의 시작을 알리는 메시지 출력
+	std::cout << "Hello, RPG World!" << std::endl;
+
+	std::cout << "Welcome to the Text-based RPG!" << std::endl;
+
+	std::cout << "You are in a dark forest." << std::endl;
+
+	while (true)
+	{
+		//플레이어가 입력한 명령어를 받음
+		//이동가능한 방이라면 이동, 아니라면 이동 불가 메시지 출력
+		//올바른 명령어가 아닐 경우 cin 버퍼 비우고 오류메시지 출력
+	}
+
+    return 0;
 }
