836ca6cd71679365c401834864588aa0628b6f72
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-26 21:33:49 +0900

    <feat> Initialize LevelData and add getter funcs _ experience, status

---
+++ TextlogRPG/LevelData/LevelData.cpp
@@ -0,0 +1,46 @@
+﻿#include "../../TextlogRPG/LevelData/LevelData.h"
+
+int32_t LevelData::experiencePerLevel[100];
+int16_t LevelData::statsPerLevel[100][4];
+
+LevelData::LevelData()
+{
+	//Initialize the experience and stats data
+	for (int8_t i = 0; i < 100; ++i)
+	{
+		if (i == 0)
+		{
+			experiencePerLevel[i] = InitializationData[0];
+			statsPerLevel[i][0] = InitializationData[1];
+			statsPerLevel[i][1] = InitializationData[2];
+			statsPerLevel[i][2] = InitializationData[3];
+			statsPerLevel[i][3] = InitializationData[4];
+		}
+		else
+		{
+			experiencePerLevel[i] = experiencePerLevel[i - 1] + 2000; //EXP
+			statsPerLevel[i][0] = statsPerLevel[i - 1][0] + 500; //HP
+			statsPerLevel[i][1] = statsPerLevel[i - 1][1] + 3; //ATK
+			statsPerLevel[i][2] = statsPerLevel[i - 1][2] + 2; //DEF
+			statsPerLevel[i][3] = statsPerLevel[i - 1][3] + 2; //AGI
+		}
+	}
+}
+
+const int32_t& LevelData::getExperienceForLevel(int16_t level)
+{
+	if (level < 1 || level > 100) return experiencePerLevel[0]; 
+	return experiencePerLevel[level - 1];
+}
+
+const int16_t* LevelData::getStatsForLevel(int16_t level)
+{
+	if (level < 1 || level > 100) return nullptr;
+	return statsPerLevel[level - 1];
+}
+
+
+LevelData::~LevelData()
+{
+
+}
---
+++ TextlogRPG/LevelData/LevelData.h
@@ -0,0 +1,29 @@
+﻿#pragma once  
+#include <cstdint>
+
+using namespace std;
+
+class LevelData  
+{  
+private:  
+	// Experience required for each level  
+	static int32_t experiencePerLevel[100];
+
+	// Status per level (HP, ATK, DEF, AGI)  
+	static int16_t statsPerLevel[100][4]; 
+	
+	//Initialized Data
+	const int16_t InitializationData[5] { 100, 100, 10, 10, 10 }; //EXP, HP, ATK, DEF, AGI
+public:
+	//Constructor
+	LevelData();
+	
+	// Returns the experience required for the given level
+	const int32_t& getExperienceForLevel(int16_t level);
+
+	// Returns a array pointer to the stats array for the given level
+	const int16_t* getStatsForLevel(int16_t level);
+
+	//Destructor
+	~LevelData();
+};
