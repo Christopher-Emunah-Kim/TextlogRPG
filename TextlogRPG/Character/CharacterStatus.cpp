3f91f5f7e8c507df566cc7595e07437304d13f6a
Christopher <hashin.chris@gmail.com>
2025-05-28 01:01:13 +0900

    <feat> add "CharacterStatus" class

---
+++ TextlogRPG/Character/CharacterStatus.cpp
@@ -0,0 +1,15 @@
+﻿#include "CharacterStatus.h"
+
+CharacterStatus CharacterStatus::NewStatus(int16_t atk, int16_t def, int16_t agi)
+{
+	return CharacterStatus(atk, def, agi);
+}
+
+CharacterStatus CharacterStatus::AddStatus(const CharacterStatus& other) const
+{
+	return CharacterStatus(
+		this->attack + other.attack,
+		this->defense + other.defense,
+		this->agility + other.agility
+	);
+}
---
+++ TextlogRPG/Character/CharacterStatus.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <cstdint>
+
+class CharacterStatus
+{
+private:
+	const int16_t attack;
+	const int16_t defense;
+	const int16_t agility;
+
+private:
+	CharacterStatus(int16_t atk, int16_t def, int16_t agi)
+		: attack{ atk }, defense{ def }, agility{ agi } {
+	}
+public:
+	static CharacterStatus NewStatus(int16_t atk, int16_t def, int16_t agi);
+
+	CharacterStatus AddStatus(const CharacterStatus& other) const;
+	
+};
+
