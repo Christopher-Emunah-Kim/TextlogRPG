48bc18478b61aed26778b776def9d83c7a1a3678
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-27 15:25:19 +0900

    <feat> add "BaseCharacter" Members

--- TextlogRPG/Character/BaseCharacter.cpp
+++ TextlogRPG/Character/BaseCharacter.cpp
@@ -1,1 +1,28 @@
 ﻿#include "BaseCharacter.h"
+
+int16_t BaseCharacter::GetLevel(BaseCharacter* character) const
+{
+	return character->characterLevel;
+}
+
+int32_t BaseCharacter::GetHealth(BaseCharacter* character) const
+{
+	return character->characterHealth;
+}
+
+int16_t BaseCharacter::GetAttack(BaseCharacter* character) const
+{
+    return character->characterstats[0];
+}
+
+int16_t BaseCharacter::GetDefense(BaseCharacter* character) const
+{
+	return character->characterstats[1];
+}
+
+int16_t BaseCharacter::GetAgility(BaseCharacter* character) const
+{
+	return character->characterstats[2];
+}
+
+
--- TextlogRPG/Character/BaseCharacter.h
+++ TextlogRPG/Character/BaseCharacter.h
@@ -1,5 +1,73 @@
 ﻿#pragma once
-class BaseCharacter
+#include <string>
+#include <cstdint>	
+#include <iostream>
+
+/* 
+	[BaseCharacter]
+	0.0 class BaseCharacter <abstract>
+	0.0.0 int16 level
+	0.0.1 int32 health
+	0.0.2 int32 maxHealth
+	0.0.3 string characterName
+	0.0.4 int16 stats[3] #Attack, Defense, Agility
+	0.0.5 void Heal(int32 amount) = 0
+	0.0.6 void TakeDamage(int32 damage) = 0
+	0.0.7 int16 GetAttack(BaseCharacter* character) = 0
+	0.0.8 int16 GetDefense(BaseCharacter* character) = 0
+	0.0.9 int16 GetAgility(BaseCharacter* character) = 0
+
+	0.1 class Player - Derived
+	0.0.0 int32 playerExperience
+	0.0.1 int32 playerGold
+	0.0.2 Weapon* weaponEquipped
+	0.0.3 Armor* armorEquipped
+	0.0.4 void LevelUp()
+	0.0.5 void GainLoot(int32 experience, int32 gold, Item* item)
+	0.0.6 void EquipItem(Item* item)
+
+	0.2 class Monster - Derived
+	0.2.0 int32 dropExperience
+	0.2.1 vector<Item*> dropitems //weapon, armor, misc items
+	0.2.2 void DropLoot(Player* player)
+*/
+
+using namespace std;
+
+class BaseCharacter abstract
 {
+private:
+	string characterName;
+	int32_t characterHealth;
+	int32_t chracterMaxHealth;
+	int16_t characterLevel;
+	int16_t characterstats[3]; // Attack, Defense, Agility
+
+public:
+	//BaseCharacter constructor
+	BaseCharacter(const string name = " ", int32_t health = 100, int32_t maxHealth = 100, int16_t level = 1, int16_t attack = 10, int16_t defense = 10, int16_t agility = 10)
+		: characterName(name), characterHealth(health), chracterMaxHealth(maxHealth), characterLevel(level)
+	{
+		characterstats[0] = attack; 
+		characterstats[1] = defense; 
+		characterstats[2] = agility; 
+		cout << "The " << characterName << " is created! " << endl;
+	}
+	
+	//Getters for character attributes
+	virtual int16_t GetLevel(BaseCharacter* character) const;
+	virtual int32_t GetHealth(BaseCharacter* character) const;
+	virtual int16_t GetAttack(BaseCharacter* character) const;
+	virtual int16_t GetDefense(BaseCharacter* character) const;
+	virtual int16_t GetAgility(BaseCharacter* character) const;
+
+	//puer virtual functions
+	virtual BaseCharacter& SetCharacter(const string& characterName) = 0;
+	virtual void Heal(int32_t amount) = 0;
+	virtual void TakeDamage(int32_t damage) = 0;
+
+	//virtual destructor
+	virtual ~BaseCharacter() = default;
+
 };
 
