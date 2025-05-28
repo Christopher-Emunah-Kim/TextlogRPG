a4c3098763c9dabbef5926d63b17955ab316d792
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-28 16:32:04 +0900

    <docs> move folder of "GameMode" class

--- TextlogRPG/Core/GameMode.h
+++ TextlogRPG/Core/GameMode.h
@@ -19,7 +19,7 @@ class GameMode
 	EGameState gameState;
 
 public:
-	GameMode() {}
+	GameMode(EGameState gs) : gameState(gs) {}
 
 	//SetGameState
 	void SetGameState(const EGameState& gs_in)
@@ -40,12 +40,3 @@ public:
 };
 
 
-
-/*GameMode
-6.0 class GameMode
-6.0.1 Enum class EGameState
-6.0.2 EGameState currentState
-6.0.3 EGameState& GetCurrentState() const
-6.0.4 void ChangeGameState(EGameState newState)
-6.0.5 void InitializeGame()
-Game states: TITLE, VILLAGE, DUNGEON */
