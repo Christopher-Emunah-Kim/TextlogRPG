0ab6ac9f9ac3e12d06192001ff16ba6975e74acc
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-28 16:07:19 +0900

    <feat> add "Common" class for include

---
+++ TextlogRPG/Util/Common.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <stdint.h>
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <sstream>
+#include <functional>
+#include <format>
+#include <functional>
+
+#include <array>
+#include <vector>
+
+#include <algorithm>
+
+#include <Windows.h>
+
+using namespace std;
