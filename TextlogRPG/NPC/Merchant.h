a07f03a6e02580a449c1a28c1aab67fc0172cb5d
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-28 21:48:41 +0900

    add "Merchant" class and members

---
+++ TextlogRPG/NPC/Merchant.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "NonPlayerCharacter.h"
+
+class Merchant : public NonPlayerCharacter {
+private:
+    vector<Item*> itemsForSale;
+    map<Item*, int32_t> itemPrices;
+
+public:
+    explicit Merchant(const string& name)
+        : NonPlayerCharacter(name, NPCType::MERCHANT) {}
+
+    void AddItemForSale(Item* item, int32_t price);
+    void SellItem(Player* player, Item* item);
+    void BuyItem(Player* player, Item* item);
+
+    // NPC 상호작용 구현
+    void Interact(Player* player) override;
+};
