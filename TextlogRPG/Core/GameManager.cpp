6f07fa2d4207c9d600a0771ef6a6b0aac97773a3
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-28 16:32:23 +0900

    <feat> add "GameManager" class

---
+++ TextlogRPG/Core/GameManager.cpp
@@ -0,0 +1,57 @@
+﻿#include "GameManager.h"
+#include "../Dialog/Dialog.h"
+#include "../Dialog/Option.h"
+
+
+void GameManager::Run() {
+    InitializeGame();
+    while (gameMode.GetGameState() != EGameState::GAME_OVER) 
+	{
+        switch (gameMode.GetGameState()) 
+		{
+            case EGameState::TITLE: RunProcessTitle(); break;
+            case EGameState::VILLAGE: RunProcessVillage(); break;
+            case EGameState::HEALER: RunProcessHealer(); break;
+            case EGameState::MERCHANT: RunProcessMerchant(); break;
+            case EGameState::DUNGEON: RunProcessDungeon(); break;
+            default: gameMode.SetGameState(EGameState::GAME_OVER); break;
+        }
+    }
+}
+
+void GameManager::InitializeGame() 
+{
+    // 플레이어 생성, 초기 상태 설정 등
+   
+
+	gameMode.SetGameState(EGameState::TITLE);
+}
+
+void GameManager::RunIntroSequence()
+{
+}
+
+void GameManager::RunProcessTitle() 
+{
+    
+}
+
+void GameManager::RunProcessVillage()
+{
+}
+
+void GameManager::RunProcessHealer()
+{
+}
+
+void GameManager::RunProcessMerchant()
+{
+}
+
+void GameManager::RunProcessDungeon()
+{
+}
+
+void GameManager::RunProcessCombat()
+{
+}
---
+++ TextlogRPG/Core/GameManager.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "../Core/GameMode.h"
+#include "../Character/Player.h"
+
+class GameManager {
+private:
+    GameMode gameMode;
+    Player* player;
+    
+    //Main Process of Game
+    void InitializeGame();
+    void RunIntroSequence();
+    void RunProcessTitle();
+    void RunProcessVillage();
+    void RunProcessHealer();
+    void RunProcessMerchant();
+    void RunProcessDungeon();
+	void RunProcessCombat();
+    
+public:
+    GameManager(const GameMode& gm, const Player& player) 
+        : gameMode(GameMode(gm.GetGameState())), player(new Player(player)) { }
+	~GameManager() { if (player) delete player; }
+    
+    void Run();
+};
