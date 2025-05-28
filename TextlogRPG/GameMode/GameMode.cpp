b6703ae6f7425598f8de0ddb0598f8924ad7e9c2
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-28 16:10:07 +0900

    <feat> update "GameMode" class for managing game state

--- TextlogRPG/GameMode/GameMode.cpp
+++ TextlogRPG/GameMode/GameMode.cpp
@@ -1,1 +1,16 @@
 ﻿#include "GameMode.h"
+
+string GameMode::GetStateString() const
+{
+	switch (gameState) 
+	{
+		case EGameState::TITLE: return "타이틀"; break;
+		case EGameState::VILLAGE: return "마을"; break;
+		case EGameState::HEALER: return "힐러"; break;
+		case EGameState::MERCHANT: return "상인"; break;
+		case EGameState::DUNGEON: return "던전"; break;
+		case EGameState::BATTLE: return "전투"; break;
+		case EGameState::GAME_OVER: return "게임 오버"; break;
+		default: return "알 수 없음"; break;
+	}
+}
--- TextlogRPG/GameMode/GameMode.h
+++ TextlogRPG/GameMode/GameMode.h
@@ -1,14 +1,5 @@
 ﻿#pragma once
-
-
-	/*GameMode
-	6.0 class GameMode 
-	6.0.1 Enum class EGameState
-	6.0.2 EGameState currentState
-	6.0.3 EGameState& GetCurrentState() const
-	6.0.4 void ChangeGameState(EGameState newState)
-	6.0.5 void InitializeGame()
-	Game states: TITLE, VILLAGE, DUNGEON */
+#include "../Util/Common.h"
 
 
 enum class EGameState
@@ -17,11 +8,15 @@ enum class EGameState
 	TITLE,
 	VILLAGE,
 	DUNGEON,
+	HEALER,
+	MERCHANT,
+	BATTLE,
+	GAME_OVER
 };
 
 class GameMode
 {
-	EGameState gameState = EGameState::NONE;
+	EGameState gameState;
 
 public:
 	GameMode() {}
@@ -32,12 +27,25 @@ public:
 		gameState = gs_in;
 	}
 	//GetGameState
-	EGameState& GetGameState()
+	EGameState GetGameState() const
 	{
 		return gameState;
 	}
+
+	string GetStateString() const;
+
 
 
 	~GameMode() {}
 };
 
+
+
+/*GameMode
+6.0 class GameMode
+6.0.1 Enum class EGameState
+6.0.2 EGameState currentState
+6.0.3 EGameState& GetCurrentState() const
+6.0.4 void ChangeGameState(EGameState newState)
+6.0.5 void InitializeGame()
+Game states: TITLE, VILLAGE, DUNGEON */
