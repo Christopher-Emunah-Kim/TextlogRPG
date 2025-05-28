b080f9ade42457989c74239183888ce4fb3c843b
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-28 21:48:27 +0900

    <feat> add "Healer" class and members

---
+++ TextlogRPG/NPC/Healer.cpp
@@ -0,0 +1,20 @@
+﻿#include "Healer.h"
+
+void Healer::HealPlayer(Player* player, int32_t cost)
+{
+	if (player->GetPlayerData().playerGold >= cost) {
+		int32_t healAmount = player->GetCharacterInfo().maxHealth - player->GetCharacterInfo().health;
+		player->Heal(healAmount);
+		player->UseGold(cost);
+		cout << "[System] " << healAmount << "의 체력을 회복했습니다." << endl;
+	}
+	else {
+		cout << "[System] 골드가 부족합니다." << endl;
+	}
+}
+
+void Healer::Interact(Player* player)
+{
+	cout << "[System] 힐러 " << npcName << "와 상호작용합니다." << endl;
+	HealPlayer(player, healCost);
+}
---
+++ TextlogRPG/NPC/Healer.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "NonPlayerCharacter.h"
+
+class Healer : public NonPlayerCharacter {
+private:
+    int32_t healCost;
+
+public:
+	explicit Healer(const string& name, int32_t cost)
+		: NonPlayerCharacter(), healCost(cost) { SetName(name);	}
+
+    int32_t GetHealCost() const { return healCost; }
+
+    void HealPlayer(Player* player, int32_t cost);
+
+    // Interact with Player
+    void Interact(Player* player) override;
+};
