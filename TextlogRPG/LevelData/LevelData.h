8f2e26ad4fe92be6afc52fc995a08fdcd1f2c7b7
Christopher <hashin.chris@gmail.com>
2025-05-30 02:44:11 +0900

    <fix> reorganize "LevelData" class

--- TextlogRPG/LevelData/LevelData.h
+++ TextlogRPG/LevelData/LevelData.h
@@ -1,28 +1,30 @@
 ﻿#pragma once  
 #include <cstdint>
+#include <iostream>
+#include <vector>
 
 using namespace std;
+
+struct FLevelProperties
+{
+	int32_t maxExperiencePerLevel;
+	int32_t maxHealthPerLevel;
+	int16_t attackPerLevel;
+	int16_t defensePerLevel;
+	int16_t agilityPerLevel;
+};
+
+static FLevelProperties levelData[100]; // Array to hold level data for 100 levels
 
 class LevelData  
 {  
 private:  
-	// Experience required for each level  
-	static int32_t experiencePerLevel[100];
 
-	// Status per level (HP, ATK, DEF, AGI)  
-	static int16_t statsPerLevel[100][4]; 
-	
-	//Initialized Data
-	const int16_t InitializationData[5] { 100, 100, 10, 10, 10 }; //EXP, HP, ATK, DEF, AGI
 public:
 	//Constructor
 	LevelData();
 	
-	// Returns the experience required for the given level
-	const int32_t& getExperienceForLevel(int16_t level);
-
-	// Returns a array pointer to the stats array for the given level
-	const int16_t* getStatsForLevel(int16_t level);
+	FLevelProperties GetLevelData(int16_t level);
 
 	//Destructor
 	~LevelData();
