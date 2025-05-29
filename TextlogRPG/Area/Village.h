189c0822a0e6194f5f097dc7468d411bc1e72e0e
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-29 18:14:17 +0900

    <feat> add "Village" class and member functions

---
+++ TextlogRPG/Area/Village.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Area.h"
+
+class NonPlayerCharacter;
+
+class Village : public Area 
+{
+private:
+    vector<NonPlayerCharacter*> npcCharacters;
+public:
+    string GetAreaName() const override { return "Village"; }
+
+    void Enter(Player* player) override;
+
+    void AddNPC(NonPlayerCharacter* npc);
+
+    void RemoveNPC(NonPlayerCharacter* npc);
+
+	vector<NonPlayerCharacter*>& GetNPCList();
+
+    void InteractWithNPC(Player* player, NonPlayerCharacter* npc);
+    
+};
