a149e360d0519b31219b3a35d949d2195bc31cf6
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-29 10:05:57 +0900

    <fix> fix interaction with "Healer" class

--- TextlogRPG/NPC/Healer.cpp
+++ TextlogRPG/NPC/Healer.cpp
@@ -2,19 +2,75 @@
 
 void Healer::HealPlayer(Player* player, int32_t cost)
 {
-	if (player->GetPlayerData().playerGold >= cost) {
+	if (player->GetPlayerData().playerGold >= cost) 
+	{
 		int32_t healAmount = player->GetCharacterInfo().maxHealth - player->GetCharacterInfo().health;
 		player->Heal(healAmount);
 		player->UseGold(cost);
-		cout << "[System] " << healAmount << "의 체력을 회복했습니다." << endl;
+		cout << "\n===========================================\n" << endl;
+		cout << "[System] 힐러 " << npcInfo.npcName << "가 치유를 시작합니다." << endl;
+		cout << "===========================================\n" << endl;
+		Sleep(2000);
+		system("cls");
+		cout << "\n===========================================\n" << endl;
+		cout << "[System] 치유가 완료되었습니다. \n" << healAmount << "의 체력이 회복되었습니다.\n" << endl;
+		cout << "[System] 치유의 집을 떠나 길거리로 나갑니다.\n" << endl;
+		cout << "===========================================\n" << endl;
+		Sleep(2000);
+		system("cls");
 	}
-	else {
-		cout << "[System] 골드가 부족합니다." << endl;
+	else 
+	{
+		cout << "\n===========================================\n" << endl;
+		cout << "[System] 골드가 부족하네요... \n" << endl;
+		cout << "===========================================\n" << endl;
+		Sleep(2000);
+		system("cls");
 	}
 }
 
 void Healer::Interact(Player* player)
 {
-	cout << "[System] 힐러 " << npcName << "와 상호작용합니다." << endl;
-	HealPlayer(player, healCost);
+	cout << "\n===========================================\n" << endl;
+	cout << "[System] 치유의 집에서 힐러 " << npcInfo.npcName << "를 만났습니다.\n" << endl;
+	cout << "===========================================\n" << endl;
+	//TODO : 힐러 만나기 로직 구현
+	Sleep(2000);
+	system("cls");
+	cout << "\n===========================================\n" << endl;
+	cout << "당신이 바로 말로만 듣던 바로 그.. " << player->GetName() << " 용사님이군요!\n" << endl;
+	cout << "안색이 좋지 않네요. 치유가 필요하신가요?\n" << endl;
+	cout << "1. 네, 치유해주세요.(" << healCost << "골드)\n2.아니요, 괜찮습니다.\n" << endl;
+	cout << "===========================================\n" << endl;
+	char healerChoice;
+	cin >> healerChoice;
+	cin.ignore();
+	Sleep(2000);
+	system("cls");
+	if (healerChoice == '1')
+	{
+		HealPlayer(player, healCost);
+		Sleep(2000);
+		system("cls");
+		
+	}
+	else if (healerChoice == '2')
+	{
+		cout << "\n===========================================\n" << endl;
+		cout << "[System] 치유를 거부하셨습니다.\n치유의 집을 떠나 길거리로 나갑니다." << endl;
+		cout << "===========================================\n" << endl;
+		Sleep(2000);
+		system("cls");
+	}
+	else
+	{
+		//TODO : 잘못된 선택 처리 로직 구현
+		cout << "\n===========================================\n" << endl;
+		cout << "[System] 잘못된 선택입니다. 치유의 집을 떠나 길거리로 나갑니다.\n" << endl;
+		cout << "===========================================\n" << endl;
+		Sleep(2000);
+		system("cls");
+	}
+	
 }
+
--- TextlogRPG/NPC/Merchant.cpp
+++ TextlogRPG/NPC/Merchant.cpp
@@ -1,29 +1,29 @@
-﻿#include "Merchant.h"
-
-void Merchant::AddItemForSale(Item* item, int32_t price) {
-    itemsForSale.push_back(item);
-    itemPrices[item] = price;
-}
-
-void Merchant::SellItem(Player* player, Item* item) {
-    auto it = itemPrices.find(item);
-    if (it != itemPrices.end() && player->GetPlayerData().playerGold >= it->second) {
-        player->UseGold(it->second);
-        player->EquipItem(item);
-        cout << "[System] " << item->getItemName() << "을(를) 구매했습니다." << endl;
-    } else {
-        cout << "[System] 골드가 부족하거나 판매하지 않는 아이템입니다." << endl;
-    }
-}
-
-void Merchant::BuyItem(Player* player, Item* item) {
-    int32_t price = item->getItemCost() / 2; // 예시: 판매가는 원가의 50%
-    player->EarnGold(price);
-    player->LoseItem(item);
-    cout << "[System] " << item->getItemName() << "을(를) " << price << "골드에 판매했습니다." << endl;
-}
-
-void Merchant::Interact(Player* player) {
-    cout << "[System] 상인 " << npcName << "와 상호작용합니다." << endl;
-    // 실제 게임에서는 메뉴를 띄워서 SellItem/BuyItem 호출
-}
+﻿//#include "Merchant.h"
+//
+//void Merchant::AddItemForSale(Item* item, int32_t price) {
+//    itemsForSale.push_back(item);
+//    itemPrices[item] = price;
+//}
+//
+//void Merchant::SellItem(Player* player, Item* item) {
+//    auto it = itemPrices.find(item);
+//    if (it != itemPrices.end() && player->GetPlayerData().playerGold >= it->second) {
+//        player->UseGold(it->second);
+//        player->EquipItem(item);
+//        cout << "[System] " << item->getItemName() << "을(를) 구매했습니다." << endl;
+//    } else {
+//        cout << "[System] 골드가 부족하거나 판매하지 않는 아이템입니다." << endl;
+//    }
+//}
+//
+//void Merchant::BuyItem(Player* player, Item* item) {
+//    int32_t price = item->getItemCost() / 2; // 예시: 판매가는 원가의 50%
+//    player->EarnGold(price);
+//    player->LoseItem(item);
+//    cout << "[System] " << item->getItemName() << "을(를) " << price << "골드에 판매했습니다." << endl;
+//}
+//
+//void Merchant::Interact(Player* player) {
+//    cout << "[System] 상인 " << npcInfo.npcName << "와 상호작용합니다." << endl;
+//    // 실제 게임에서는 메뉴를 띄워서 SellItem/BuyItem 호출
+//}
