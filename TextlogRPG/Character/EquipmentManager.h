e78f99070a8d22b1e984132322c30789e012ca6a
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-05 15:51:20 +0900

    <feat> add "EquipmentManager" class for managing items of player

---
+++ TextlogRPG/Character/EquipmentManager.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "../Item/Weapon.h"
+#include "../Item/Armor.h"
+#include "../Item/MiscItem.h"
+
+class EquipmentManager 
+{
+private:
+	Weapon* pWeapon = nullptr;
+	Armor* pArmor = nullptr;
+	MiscItem* pMiscItem = nullptr;
+
+public:
+    void Equip(Item* item);
+
+    void Unequip(EItemType type);
+
+    Weapon* GetWeapon() const;
+
+    Armor* GetArmor() const;
+
+    MiscItem* GetMisc() const;
+
+};
