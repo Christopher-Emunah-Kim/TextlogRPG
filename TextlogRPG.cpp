8449711e0e4f30bab40a0f59f367d162af073e0d
Christopher <hashin.chris@gmail.com>
2025-06-07 22:41:30 +0900

    <fix> fix Language Encoding

--- TextlogRPG.cpp
+++ TextlogRPG.cpp
@@ -7,7 +7,13 @@
 using namespace std;
 
 
-int32 main(){
+int32 main()
+{
+	// 로케일 설정-한글출력지원
+	locale::global(locale("kor"));
+	wcin.imbue(locale());
+	wcout.imbue(locale());
+
 	//Game Start Settings
 	Player* player = Player::CreateCharacter("NONE");
 	EGameState initialState = EGameState::TITLE;
--- TextlogRPG/Item/Weapon.cpp
+++ TextlogRPG/Item/Weapon.cpp
@@ -8,13 +8,5 @@
 //TODO : !!!!!! 반드시 의존역전관계 해결(ITEM클래스들)
 void Weapon::EquippedBy(Player* player)
 {
-	/*if (player == nullptr) return;
-	CharacterStatus status = CharacterStatus::NewStatus(
-		GetItemInfo().attack,
-		GetItemInfo().defense,
-		GetItemInfo().agility
-	);
-
-	player->GetCharacterInfo().characterStats = status.AddStatus(status);*/
-
+	
 }
