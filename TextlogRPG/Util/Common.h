5c278104e3b7e8993d7b1bf340bba203c1392757
Christopher <hashin.chris@gmail.com>
2025-06-07 15:53:43 +0900

    <fix> fix the data type

--- TextlogRPG/Util/Common.h
+++ TextlogRPG/Util/Common.h
@@ -1,13 +1,13 @@
 ﻿#pragma once
 
-typedef signed char			 int8;     //-128~127
-typedef short				 int16;   //-32768~32767
-typedef int					 int32;   //-2147483648~2147483647
-typedef long long			 int64;   //-9223372036854775808~9223372036854775807
-typedef unsigned char	     uint8;   //0~255
-typedef unsigned short       uint16;  //0~65535
-typedef unsigned int         uint32;  //0~4294967295
-typedef unsigned long long	 uint64;  //0~18446744073709551615
+typedef signed __int8			 int8;     //-128~127
+typedef signed __int16		 int16;   //-32768~32767
+typedef signed __int32		 int32;   //-2147483648~2147483647
+typedef signed __int64		 int64;   //-9223372036854775808~9223372036854775807
+typedef unsigned __int8      uint8;   //0~255
+typedef unsigned __int16     uint16;  //0~65535
+typedef unsigned __int32     uint32;  //0~4294967295
+typedef unsigned __int64     uint64;  //0~18446744073709551615
 
 
 #include <iostream>
