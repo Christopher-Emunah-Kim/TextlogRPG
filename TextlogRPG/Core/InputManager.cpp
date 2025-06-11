2a2e701676fc60062766fa79e48799c2c32793c9
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-11 15:05:22 +0900

    <feat> add InputManger class and members

---
+++ TextlogRPG/Core/InputManager.cpp
@@ -0,0 +1,27 @@
+﻿#include "InputManager.h"
+#include <iostream>
+#include <limits>
+
+InputManager& InputManager::GetInstance()
+{
+    static InputManager instance;
+
+    return instance;
+}
+
+char InputManager::GetCharInput()
+{
+    char ch;
+    cin >> ch;
+    cin.ignore(1024, '\n'); 
+
+    return ch;
+}
+
+string InputManager::GetLineInput()
+{
+    string line;
+    getline(std::cin, line);
+
+    return line;
+}
---
+++ TextlogRPG/Core/InputManager.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <string>
+
+using namespace std;
+
+
+class InputManager
+{
+private:
+	InputManager() = default;
+
+
+public:
+
+    static InputManager& GetInstance();
+
+    char GetCharInput();
+
+    string GetLineInput();
+
+    
+};
