ff1ac5fc5d86596e0eb2cb1e13b060467890f516
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-12 11:57:02 +0900

    <feat> add PlayerInventoryComponent class and members

---
+++ TextlogRPG/Character/Component/PlayerInventoryComponent.cpp
@@ -0,0 +1,69 @@
+﻿#include "PlayerInventoryComponent.h"
+#include "../Player.h"
+#include "../../Item/Item.h"
+
+PlayerInventoryComponent::PlayerInventoryComponent(Player* owner)
+    : m_owner(owner)
+{
+}
+
+void PlayerInventoryComponent::Initialize()
+{
+    
+}
+
+void PlayerInventoryComponent::Update()
+{
+    
+}
+
+void PlayerInventoryComponent::SetOwner(Player* owner)
+{
+	m_owner = owner;
+}
+
+void PlayerInventoryComponent::AddItem(Item* item)
+{
+    if (item == nullptr)
+        return;
+        
+	m_inventoryManager.AddItem(item);
+    RenderItemAdded(item);
+}
+
+void PlayerInventoryComponent::RemoveItem(Item* item)
+{
+    if (item == nullptr)
+        return;
+        
+	m_inventoryManager.RemoveItem(item);
+    RenderItemRemoved(item);
+}
+
+list<Item*> PlayerInventoryComponent::GetItemsByType(EItemType type) const
+{
+    return m_inventoryManager.GetItemsByType(type);
+}
+
+const list<Item*>& PlayerInventoryComponent::GetAllItems() const
+{
+    return m_inventoryManager.GetAllItems();
+}
+
+void PlayerInventoryComponent::RenderItemAdded(Item* item)
+{
+    if (item != nullptr)
+    {
+        string message = item->GetItemInfo().itemName + "을(를) 인벤토리에 추가했습니다.";
+        Common::PrintSystemMsg(message);
+    }
+}
+
+void PlayerInventoryComponent::RenderItemRemoved(Item* item)
+{
+    if (item != nullptr)
+    {
+        string message = item->GetItemInfo().itemName + "을(를) 인벤토리에서 제거했습니다.";
+        Common::PrintSystemMsg(message);
+    }
+}
---
+++ TextlogRPG/Character/Component/PlayerInventoryComponent.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "IComponent.h"
+#include "../../Core/InventoryManager.h"
+#include "../../Util/Common.h"
+
+class Player;
+
+
+class PlayerInventoryComponent : public IInventoryComponent
+{
+public:
+    PlayerInventoryComponent(Player* owner = nullptr);
+
+    virtual ~PlayerInventoryComponent() = default;
+    
+private:
+	class Player* m_owner;
+	InventoryManager m_inventoryManager;
+
+public:
+    virtual void Initialize() override;
+    virtual void Update() override;
+    virtual void SetOwner(Player* owner) override;
+    
+public:
+    virtual void AddItem(Item* item) override;
+    virtual void RemoveItem(Item* item) override;
+    virtual list<Item*> GetItemsByType(EItemType type) const override;
+    virtual const list<Item*>& GetAllItems() const override;
+
+    virtual void RenderItemAdded(Item* item) override;
+    virtual void RenderItemRemoved(Item* item) override;
+    
+};
