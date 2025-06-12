39aaa60acd67060160c814cd1dc57fbd2a2083d3
Christopher <hashin.chris@gmail.com>
2025-06-13 02:19:59 +0900

    <perf> refactoring GameManger class to Title class

--- TextlogRPG/Area/Title.cpp
+++ TextlogRPG/Area/Title.cpp
@@ -1,7 +1,78 @@
 ﻿#include "Title.h"
 #include "../Character/Player.h"
+#include "../Util/EGameState.h"
+
+Title::Title()
+{
+}
+
+Title::~Title()
+{
+}
 
 void Title::Enter(Player* player)
+{
+	RenderPlayerGreeting(player);
+}
+
+void Title::Initialize(Player* player)
+{
+
+}
+
+EGameState Title::Process(Player* player)
+{
+	Enter(player);
+
+	char menuChoice = Common::GetCharInput();
+
+	return HandleChoice(menuChoice, player);
+}
+
+EGameState Title::HandleChoice(char menuChoice, Player* player)
+{
+	switch (menuChoice)
+	{
+	case '1':
+	{
+		return EGameState::VILLAGE;
+	}
+	break;
+	case '2':
+	{
+		return EGameState::DUNGEON;
+	}
+	break;
+	case '3':
+	{
+		if (player == nullptr)
+		{
+			Common::PrintErrorMsg("플레이어 정보가 없습니다.");
+		}
+		else
+		{
+			Common::PauseAndClearScreen();
+			player->RenderPlayerStatus();
+		}
+		return EGameState::TITLE;
+	}
+	break;
+	case '4':
+	{
+		return EGameState::GAME_OVER;
+	}
+	break;
+
+	default:
+	{
+		Common::PrintErrorMsg("잘못된 입력입니다.");
+		return EGameState::TITLE; 
+	}
+	break;
+	}
+}
+
+void Title::RenderPlayerGreeting(Player* player)
 {
 	string strWelcomeMsg = "환영합니다, 용사 " + player->GetName() + " 님!\n이제 당신의 모험이 시작됩니다!!";
 
--- TextlogRPG/Area/Title.h
+++ TextlogRPG/Area/Title.h
@@ -1,9 +1,25 @@
 ﻿#pragma once
 #include "Area.h"
 
+enum class EGameState;
+
 class Title : public Area 
 {
 public:
+	Title();
+	virtual ~Title();
+
+public:
 	virtual string GetAreaName() const override { return "Title"; }
 	virtual void Enter(Player* player) override;
+
+public:
+	void Initialize(Player* player);
+	EGameState Process(Player* player);
+
+private:
+	EGameState HandleChoice(char menuChoice, Player* player);
+
+	void RenderPlayerGreeting(Player* player);
+
 };
