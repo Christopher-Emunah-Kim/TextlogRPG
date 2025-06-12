ad46eab964356e32a54db1b5cf018d21750ec62e
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-12 16:57:23 +0900

    <feat> add MonsterCombatComponent class for Monster class

---
+++ TextlogRPG/Character/Component/MonsterCombatComponent.cpp
@@ -0,0 +1,114 @@
+﻿#include "MonsterCombatComponent.h"
+#include "MonsterStatusComponent.h"
+#include "../Monster.h"
+
+MonsterCombatComponent::MonsterCombatComponent(Monster* owner)
+    : m_owner(owner), m_lastCalculatedDamage(0)
+{
+}
+
+MonsterCombatComponent::~MonsterCombatComponent()
+{
+}
+
+void MonsterCombatComponent::Initialize()
+{
+    m_lastCalculatedDamage = 0;
+}
+
+void MonsterCombatComponent::Update()
+{
+}
+
+void MonsterCombatComponent::SetOwner(BaseCharacter* owner)
+{
+	m_owner = dynamic_cast<Monster*>(owner);
+}
+
+void MonsterCombatComponent::Attack(BaseCharacter* target)
+{
+	if (target == nullptr || m_owner == nullptr)
+	{
+		Common::PrintErrorMsg("오류 발생. 공격할 수 없습니다.");
+		return;
+	}
+    
+    RenderAttackMessage(target);
+    UpdateAttack(target);
+}
+
+void MonsterCombatComponent::ApplyDamageFrom(BaseCharacter& attacker)
+{
+    if (m_owner==nullptr) 
+		return;
+
+    m_lastCalculatedDamage = UpdateDamage(attacker);
+    
+    // 데미지 결과 렌더링은 MonsterStatusComponent에서 처리
+	MonsterStatusComponent& statusComp = m_owner->GetMonsterStatusComponent();
+    
+	statusComp.RenderDamageResult(m_lastCalculatedDamage);
+    
+    Common::PauseAndClearScreen(2000);
+}
+
+int32 MonsterCombatComponent::CalculateDamageFrom(const FCharacterInfo& targetCharacterInfo)
+{
+	if (m_owner == nullptr)
+	{
+		return 0;
+	}
+    
+    const FCharacterInfo& monsterInfo = m_owner->GetCharacterInfo();
+    return monsterInfo.characterStats.CalculateDamage(targetCharacterInfo.characterStats);
+}
+
+int32 MonsterCombatComponent::UpdateDamage(BaseCharacter& attacker)
+{
+	if (m_owner == nullptr)
+	{
+		return 0;
+	}
+    
+    const FCharacterInfo& attackerInfo = attacker.GetCharacterInfo();
+    int32 damage = CalculateDamageFrom(attackerInfo);
+    
+    // 몬스터 체력 업데이트
+	MonsterStatusComponent& statusComp = m_owner->GetMonsterStatusComponent();
+    
+	const FCharacterInfo& monsterInfo = m_owner->GetCharacterInfo();
+
+	int32 currentHealth = monsterInfo.iCurrentHealth;
+	int32 calculatedHealth = currentHealth - damage;
+	m_owner->SetCurrentHealth(calculatedHealth);
+
+	if (monsterInfo.iCurrentHealth <= 0)
+	{
+		statusComp.SetCurrentHealth(0);
+	}
+    
+    return damage;
+}
+
+void MonsterCombatComponent::UpdateAttack(BaseCharacter* target)
+{
+    if (target && m_owner)
+    {
+        target->ApplyDamageFrom(*m_owner);
+    }
+}
+
+void MonsterCombatComponent::RenderAttackMessage(BaseCharacter* target)
+{
+    if (m_owner)
+    {
+        string msg = m_owner->GetCharacterInfo().strCharacterName + "이(가) 당신을 공격합니다.";
+        Common::PrintSystemMsg(msg);
+        Common::PauseAndClearScreen(3000);
+    }
+}
+
+void MonsterCombatComponent::RenderDamageResult(BaseCharacter& attacker, int32 damage)
+{
+    // MonsterStatusComponent에서 처리
+}
---
+++ TextlogRPG/Character/Component/MonsterCombatComponent.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "IComponent.h"
+
+class Monster;
+class BaseCharacter;
+
+class MonsterCombatComponent : public ICombatComponent
+{
+public:
+    MonsterCombatComponent(Monster* owner = nullptr);
+    virtual ~MonsterCombatComponent();
+
+private:
+	Monster* m_owner;
+	int32 m_lastCalculatedDamage;
+
+public:
+    virtual void Initialize() override;
+    virtual void Update() override;
+    virtual void SetOwner(BaseCharacter* owner);
+    
+public:
+    virtual void Attack(BaseCharacter* target) override;
+    virtual void ApplyDamageFrom(BaseCharacter& attacker) override;
+    virtual int32 CalculateDamageFrom(const FCharacterInfo& targetCharacterInfo) override;
+    virtual int32 UpdateDamage(BaseCharacter& attacker) override;
+    virtual void UpdateAttack(BaseCharacter* target) override;
+    virtual void RenderAttackMessage(BaseCharacter* target) override;
+    virtual void RenderDamageResult(BaseCharacter& attacker, int32 damage) override;
+
+
+	// Don't Use virtual functions
+#pragma region UNUSED FUNCS
+    virtual void DisplayDeathMessage() override {}
+    virtual void DisplayDamageMessage(BaseCharacter& attacker, int32 damage) override {}
+#pragma endregion
+
+};
