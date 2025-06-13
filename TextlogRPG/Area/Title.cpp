cf16850ed20a6175523a2d9d6154ff6bf434edcf
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-13 14:42:39 +0900

    <fix> adapt GameLoop pattern in Title class

--- TextlogRPG/Area/Title.cpp
+++ TextlogRPG/Area/Title.cpp
@@ -12,7 +12,12 @@ Title::~Title()
 
 void Title::Enter(Player* player)
 {
+	m_player = player;
+	m_shouldShowMenu = true;
+	m_shouldShowStatus = false;
+
 	RenderPlayerGreeting(player);
+	Render();
 }
 
 void Title::Initialize(Player* player)
@@ -22,11 +27,110 @@ void Title::Initialize(Player* player)
 
 EGameState Title::Process(Player* player)
 {
-	Enter(player);
+	/*Enter(player);
 
 	char menuChoice = Common::GetCharInput();
 
-	return HandleChoice(menuChoice, player);
+	return HandleChoice(menuChoice, player);*/
+
+	return Run(player);
+}
+
+void Title::ProcessInput(Player* player)
+{
+	if (m_shouldShowMenu)
+	{
+		m_input = Common::GetCharInput();
+	}
+}
+
+void Title::Update(float deltaTime)
+{
+	m_accumulatedTime += deltaTime;
+
+	if (m_input != 0)
+	{
+		switch (m_input)
+		{
+		case '1':
+		{
+			m_nextState = EGameState::VILLAGE;
+			m_isRunning = false;
+		}
+		break;
+
+		case '2':
+		{
+			m_nextState = EGameState::DUNGEON;
+			m_isRunning = false;
+		}
+		break;
+
+		case '3':
+		{
+			if (m_player != nullptr)
+			{
+				m_shouldShowMenu = false;
+				m_shouldShowStatus = true;
+				Common::PauseAndClearScreen();
+				m_player->RenderPlayerStatus();
+
+				m_accumulatedTime = 0.0f;
+			}
+			else
+			{
+				Common::PrintErrorMsg("플레이어 정보가 없습니다.");
+			}
+		}
+		break;
+
+		case '4':
+		{
+			m_nextState = EGameState::GAME_OVER;
+			m_isRunning = false;
+		}
+		break;
+
+		default:
+		{
+			Common::PrintErrorMsg("잘못된 입력입니다.");
+		}
+		break;
+		}
+
+		//초기화.
+		m_input = 0;
+		
+	}
+
+	if (m_shouldShowStatus && m_accumulatedTime > 1.0f)
+	{
+		m_shouldShowStatus = false;
+		m_shouldShowMenu = true;
+		m_accumulatedTime = 0.0f;
+		Common::PauseAndClearScreen(1000);
+		Render();
+	}
+}
+
+void Title::Render()
+{
+	if (m_shouldShowMenu)
+	{
+		Common::PauseAndClearScreen();
+
+		Common::PrintSystemMsg("타이틀 화면입니다.\n\n1. 마을로 가기\n\n2. 던전에 가기\n\n3. 플레이어 정보 확인하기 \n\n4. 게임을 떠나기");
+	}
+}
+
+bool Title::ShouldExit()
+{
+	return !m_isRunning;
+}
+
+EGameState Title::GetNextState()
+{
+	return m_nextState;
 }
 
 EGameState Title::HandleChoice(char menuChoice, Player* player)
@@ -74,14 +178,12 @@ EGameState Title::HandleChoice(char menuChoice, Player* player)
 
 void Title::RenderPlayerGreeting(Player* player)
 {
-	string strWelcomeMsg = "환영합니다, 용사 " + player->GetName() + " 님!\n이제 당신의 모험이 시작됩니다!!";
-
-	Common::PrintSystemMsg(strWelcomeMsg);
+	Common::PrintSystemMsg("환영합니다, 용사 " + player->GetName() + " 님!\n이제 당신의 모험이 시작됩니다!!");
 
 	Common::PrintSystemMsg("타이틀을 출력중입니다...");
 
-	Common::PauseAndClearScreen(2500);
+	/*Common::PauseAndClearScreen(2500);
 
-	Common::PrintSystemMsg("타이틀 화면입니다.\n\n1. 마을로 가기\n\n2. 던전에 가기\n\n3. 플레이어 정보 확인하기 \n\n4. 게임을 떠나기");
+	Common::PrintSystemMsg("타이틀 화면입니다.\n\n1. 마을로 가기\n\n2. 던전에 가기\n\n3. 플레이어 정보 확인하기 \n\n4. 게임을 떠나기");*/
 }
 
--- TextlogRPG/Area/Title.h
+++ TextlogRPG/Area/Title.h
@@ -9,6 +9,11 @@ public:
 	Title();
 	virtual ~Title();
 
+private:
+	Player* m_player = nullptr;
+	bool m_shouldShowMenu = true;
+	bool m_shouldShowStatus = false;
+
 public:
 	virtual string GetAreaName() const override { return "Title"; }
 	virtual void Enter(Player* player) override;
@@ -16,6 +21,13 @@ public:
 public:
 	void Initialize(Player* player);
 	EGameState Process(Player* player);
+
+protected:
+	virtual void ProcessInput(Player* player) override;
+	virtual void Update(float deltaTime) override;
+	virtual void Render() override;
+	virtual bool ShouldExit() override;
+	virtual EGameState GetNextState() override;
 
 private:
 	EGameState HandleChoice(char menuChoice, Player* player);
