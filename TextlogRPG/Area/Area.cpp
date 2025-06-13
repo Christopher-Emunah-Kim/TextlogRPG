8c7b99b8f5a3c424e21aa6fb1e44eb0f4fd8d1ae
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-13 14:42:23 +0900

    <fix> adapt GameLoop pattern in Area class

---
+++ TextlogRPG/Area/Area.cpp
@@ -0,0 +1,30 @@
+﻿#include "Area.h"
+#include <time.h>
+
+EGameState Area::Run(Player* player)
+{
+	Enter(player);
+
+	m_isRunning = true;
+	m_nextState = EGameState::TITLE; //default
+
+	clock_t lastTime = clock();
+	float deltaTime;
+
+	//GAME LOOP
+	while (m_isRunning && ShouldExit()==false)
+	{
+		//update time
+		clock_t currentTime = clock();
+		deltaTime = static_cast<float>(currentTime - lastTime) / CLOCKS_PER_SEC;
+		lastTime = currentTime;
+
+		//LOOP
+		ProcessInput(player);
+		Update(deltaTime);
+		Render();
+	}
+
+    return GetNextState();
+}
+
--- TextlogRPG/Area/Area.h
+++ TextlogRPG/Area/Area.h
@@ -1,13 +1,37 @@
 ﻿#pragma once
 #include "../Util/Common.h"
+#include "../Util/EGameState.h"
 
 class Player;
+enum class EGameState;
 
 class Area 
 {
 public:
     virtual ~Area() = default;
 
+protected:
+	bool m_isRunning = false;
+	EGameState m_nextState = EGameState::TITLE;
+	char m_input = 0;
+	float m_accumulatedTime = 0.0f;
+
+public:
+	virtual EGameState Run(Player* player);
+
+public:
     virtual string GetAreaName() const = 0;
     virtual void Enter(Player* player) = 0;
+	
+protected:
+	//GAME LOOP
+	virtual void ProcessInput(Player* player) = 0;
+	virtual void Update(float deltaTime) = 0;
+	virtual void Render() = 0;
+
+	//EXIT CONDTIONS
+	virtual bool ShouldExit() = 0;
+	virtual EGameState GetNextState() = 0;
+
+
 };
