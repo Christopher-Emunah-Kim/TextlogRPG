5bc248ccece7e4b343e1c3d3d8bfb35af905747f
Christopher <hashin.chris@gmail.com>
2025-06-15 18:40:43 +0900

    <feat> add LevelAreaManager class for managing levels

---
+++ TextlogRPG/Area/LevelAreaManager.cpp
@@ -0,0 +1,159 @@
+﻿#include "LevelAreaManager.h"
+#include "BaseLevel.h"
+
+LevelAreaManager::LevelAreaManager()
+	: m_playerPtr(nullptr),
+	m_currentState(EGameState::TITLE) 
+{ }
+
+LevelAreaManager::~LevelAreaManager()
+{
+	m_levelAreaMap.clear();
+	m_gameObjects.clear();
+}
+
+void LevelAreaManager::Init(Player* player)
+{
+	m_playerPtr = player;
+
+	BaseLevel* currentArea = GetCurrentLevelArea();
+	if (currentArea)
+	{
+		currentArea->Enter(player);
+	}
+	else
+	{
+		Common::PrintErrorMsg("현재 LevelArea가 없습니다. 초기화를 중단합니다.");
+		return;
+	}
+
+	for (size_t i = 0; i < m_gameObjects.size(); ++i)
+	{
+		IGameObject* gameObject = m_gameObjects[i];
+		if (gameObject)
+		{
+			gameObject->Init();
+		}
+		else
+		{
+			Common::PrintErrorMsg("GameObject가 nullptr 입니다. 초기화를 중단합니다.");
+		}
+	}
+
+}
+
+void LevelAreaManager::Update(float deltaTime)
+{
+	for (size_t i = 0; i < m_gameObjects.size(); ++i)
+	{
+		IGameObject* gameObject = m_gameObjects[i];
+		if (gameObject)
+		{
+			gameObject->Update(deltaTime);
+		}
+	}
+
+	//TODO : 각 Area의 게임루프를 여기로 끄집어올리기.
+
+}
+
+void LevelAreaManager::Render()
+{
+	for (size_t i = 0; i < m_gameObjects.size(); ++i)
+	{
+		IGameObject* gameObject = m_gameObjects[i];
+		if (gameObject)
+		{
+			gameObject->Render();
+		}
+	}
+}
+
+EGameState LevelAreaManager::ProcessCurrentArea(Player* player, float deltaTime /*= 0.0f*/)
+{
+	BaseLevel* currentArea = GetCurrentLevelArea();
+
+	if (currentArea != nullptr)
+	{
+		EGameState nextState = currentArea->Run(player, deltaTime);
+		return nextState;
+	}
+	else
+	{
+		Common::PrintErrorMsg("현재 LevelArea가 nullptr 입니다. 게임 루프를 중단합니다.");
+		return EGameState::GAME_OVER;
+	}
+
+	return m_currentState;
+}
+
+EGameState LevelAreaManager::GetCurrnetState() const
+{
+	return m_currentState;
+}
+
+void LevelAreaManager::SetCurrentState(const EGameState& newState)
+{
+	m_currentState = newState;
+}
+
+void LevelAreaManager::RegisterGameObejcts(IGameObject* gameObject)
+{
+	if (gameObject != nullptr)
+	{
+		m_gameObjects.push_back(gameObject);
+	}
+	else
+	{
+		Common::PrintErrorMsg("GameObject가 nullptr 입니다. 등록을 중단합니다.");
+	}
+}
+
+void LevelAreaManager::UnregisterGameObjects(IGameObject* gameObject)
+{
+	vector<IGameObject*>::iterator it = std::find(m_gameObjects.begin(), m_gameObjects.end(), gameObject);
+	if (it != m_gameObjects.end())
+	{
+		m_gameObjects.erase(it);
+	}
+	else
+	{
+		Common::PrintErrorMsg("GameObject가 등록되어 있지 않습니다. 제거를 중단합니다.");
+	}
+}
+
+void LevelAreaManager::ClearGameObjects()
+{
+	m_gameObjects.clear();
+}
+
+BaseLevel* LevelAreaManager::GetLevelArea(const EGameState& state) const
+{
+	unordered_map<EGameState, BaseLevel*>::const_iterator it = m_levelAreaMap.find(state);
+	if (it != m_levelAreaMap.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		Common::PrintErrorMsg("해당 EGameState에 대한 LevelArea가 등록되어 있지 않습니다.");
+		return nullptr;
+	}
+}
+
+BaseLevel* LevelAreaManager::GetCurrentLevelArea() const
+{
+	return GetLevelArea(m_currentState);
+}
+
+void LevelAreaManager::RegisterLevelArea(const EGameState& state, BaseLevel* levelArea)
+{
+	if (levelArea != nullptr)
+	{
+		m_levelAreaMap[state] = levelArea;
+	}
+	else
+	{
+		Common::PrintErrorMsg("LevelArea가 nullptr 입니다. 등록을 중단합니다.");
+	}
+}
---
+++ TextlogRPG/Area/LevelAreaManager.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "../Util/Common.h"
+#include "../Util/EGameState.h"
+#include "../Core/IGameObject.h"
+
+class BaseLevel;
+class Player;
+
+class LevelAreaManager
+{
+public:
+	LevelAreaManager();
+	~LevelAreaManager();
+
+private:
+	Player*													m_playerPtr;
+	EGameState												m_currentState;
+	vector<IGameObject*>								m_gameObjects;
+	unordered_map<EGameState, BaseLevel*>		m_levelAreaMap;
+
+public: //GAME LOOP
+	void Init(Player* player);
+	void Update(float deltaTime);
+	void Render();
+
+	EGameState		ProcessCurrentArea(Player* player, float deltaTime = 0.0f);
+
+public: //Managing Game State
+	EGameState		GetCurrnetState() const;
+	void				SetCurrentState(const EGameState& newState);
+
+public: //Managing GameObjects
+	void				RegisterGameObejcts(IGameObject* gameObject);
+	void				UnregisterGameObjects(IGameObject* gameObject);
+	void				ClearGameObjects();
+
+public: //Managing LevelAreas
+	BaseLevel*		GetLevelArea(const EGameState& state) const;
+	BaseLevel*		GetCurrentLevelArea() const;
+	void				RegisterLevelArea(const EGameState& state, BaseLevel* levelArea);
+
+
+};
+
